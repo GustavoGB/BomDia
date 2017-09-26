66a535ec1c2442504e8b73c7a314f48bb77e3408
elisa <elisamalzoni@gmail.com>
2017-09-26 08:58:45 -0300

    first commit

---
+++ src/mvc/model/Message.java
@@ -0,0 +1,33 @@
+package mvc.model;
+
+public class Message {
+	private Integer id;
+	private Integer userId;
+	private String content;
+	private String toWhom;
+	private Integer hour;
+	private Integer isActive;
+	private Integer isDeleted;
+	
+	public Integer getId() {return id;}
+	public void setId(Integer id) {this.id = id;}
+	
+	public Integer getUserId() {return userId;}
+	public void setUserId(Integer userId) {this.userId = userId;}
+	
+	public String getContent() {return content;}
+	public void setContent(String content) {this.content = content;}
+	
+	public String getToWhom() {return toWhom;}
+	public void setToWhom(String toWhom) {this.toWhom = toWhom;}
+	
+	public Integer getHour() {return hour;}
+	public void setHour(Integer hour) {this.hour = hour;}
+	
+	public Integer getIsActive() {return isActive;}
+	public void setIsActive(Integer isActive) {this.isActive = isActive;}
+	
+	public Integer getIsDeleted() {return isDeleted;}	
+	public void setIsDeleted(Integer isDeleted) {this.isDeleted = isDeleted;}
+
+}
---
+++ src/mvc/model/User.java
@@ -0,0 +1,26 @@
+package mvc.model;
+
+public class User {
+	private Integer id;
+	private String phone;
+	private String password;
+//	private Blob profilePicture;
+	private String name;
+	
+	public Integer getId() {return id;}
+	public void setID(Integer id) {this.id = id;}
+
+	public String getPhone() {return phone;}
+	public void setPhone(String phone) {this.phone = phone;}
+
+	public String getPassword() {return password;}
+	public void setPassword(String password) {this.password = password;}
+
+//	public Blob getProfilePicture() {return profilePicture;}
+//	public void setProfilePicture(Blob profilePicture) {this.profilePicture = profilePicture;}
+
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+
+	
+}
